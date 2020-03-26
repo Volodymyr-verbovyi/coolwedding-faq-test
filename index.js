3d8fb38ef14b46e99831914dbb6c6bfefa321a1b
Volodymyr Verbovyi <voha@MacBook-Pro-Volodymyr.local>
2020-03-26 21:56:11 +0200

    edited bot

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
+const http = require('http');
+const https = require('https');
 
-const TOKEN= '996463033:AAEX502RCoUE3pi8M0BSUyCijBW7moLSm-U'
+const TOKEN = '996463033:AAEX502CoUE3pi8M0BSUyCijBW7moLSm-U';
 
-const bot = new TelegramBot(TOKEN,  {
+const bot = new TelegramBot(TOKEN, {
     polling: {
         interval: 300,
         autoStart: true,
@@ -10,24 +12,40 @@ const bot = new TelegramBot(TOKEN,  {
             timeout: 10
         }
     }
-})
-
-
-
-
-bot.on('message', msg => {
-
- const chadid = msg.chat.id
-
-
-
-    bot.sendMessage(chadid, 'Получили твой интерес! Спасибо!'+ msg.from.first_name, {
-        reply_markup: {
-            keyboard: [
-                ['Пакеты', 'О нас '],
-                ['Скоько опеарторов', '5'],
-                ['Цены'],
-            ]
-        }
-    } );
 });
+
+http.createServer().listen(process.env.PORT || 4000).on('request', function (req, res) {
+    res.end('')
+});
+setInterval(function () {
+    https.get('https://git.heroku.com/test-cool1-bot.git')
+}, 300000);
+
+try {
+    bot.on('message', async msg => {
+        const chadid = msg.chat.id;
+        const buttons = {
+            reply_markup: {
+                keyboard: [
+                    ['page', 'О нас '],
+                    ['Скоько опеарторов', '5'],
+                    ['Цены'],
+                ]
+            }
+        };
+
+        if (msg.text === '/start') {
+            await bot.sendMessage(chadid, `good day, ${msg.from.first_name}!`, buttons);
+        }
+        if (msg.text === 'page') {
+           await bot.sendMessage(chadid, 'this is your page', buttons);
+        } else if (msg.text === 'Цены') {
+            await bot.sendMessage(chadid, '120000', buttons);
+        }
+
+       // await bot.sendMessage(chadid, 'Получили твой интерес! Спасибо!' + msg.from.first_name, buttons);
+    });
+} catch (err) {
+    console.warn(err.message);
+}
+
